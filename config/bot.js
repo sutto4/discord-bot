a76099991c88f409ca4becddbb7b295c773cd4fe
sutto <133205772+sutto4@users.noreply.github.com>
2025-08-07 00:31:35 +1000

    Update bot.js

--- config/bot.js
+++ config/bot.js
@@ -16,7 +16,7 @@ const client = new Client({
 
 client.commands = new Collection();
 
-// Load slash commands
+// Load slash commandss
 const commandsPath = path.join(__dirname, '../commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
